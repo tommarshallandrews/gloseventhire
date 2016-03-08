74270292a07f9205fc1c2b55bf55a5c6eff76ce6
Tom Marshall-Andrews <tommarshallandrews@gmail.com>
2016-03-08 11:30:06 +0000

    bug

--- resources/views/masters/footer.blade.php
+++ resources/views/masters/footer.blade.php
@@ -45,9 +45,6 @@
     <script src="{{URL::to('/')}}/js/custom.js"></script>
 
 
-
-
-      <script src="//code.jquery.com/jquery-1.10.2.js"></script>
   <script src="//code.jquery.com/ui/1.11.2/jquery-ui.js"></script>
   <script>
   $(function() {
